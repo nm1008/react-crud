ed6d4ff72de0f22b024f29b79aa3735c64fb7b20
nm1008 <mallari08nikko@yahoo.com>
2023-09-18 08:26:30 +0800

    added input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [newTask, setNewTask] = useState("");
+ 
+  
+
+
   return (
-    <>
+    <div className="app">
       <h1>REACT CRUD</h1>
-    </>
+      <Input setNewTask={setNewTask}  />
+    </div>
+  );
+}
+
+function Input({ setNewTask }) {
+  return (
+    <div className="input">
+      <input type="text" onChange={(e) => setNewTask(e.target.value)} />
+      <button>Add to List</button>
+    </div>
   );
 }
 
